bf096a78a5c7a256497fa9d1ffdc9167c74ec0fb
quincy-soeliman <qsoeliman.dev@gmail.com>
2016-01-04 22:29:10 +0100

    Added database and API config

---
+++ app/config.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Database configuration
+ */
+$db = [
+  'db_type' => '',
+  'db_host' => '',
+  'db_name' => '',
+  'db_user' => '',
+  'db_pass' => '',
+];
+
+/**
+ * API configuration
+ */
+define('API_KEY', '912d9953-2269-4eb0-80fd-cb56f50d0fce');
+define('BASE_URL', 'https://euw.api.pvp.net');
