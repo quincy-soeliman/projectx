e1acd8d92e9df541a42cd54bac89f3a798d7b3c5
quincy-soeliman <qsoeliman.dev@gmail.com>
2016-01-04 23:48:47 +0100

    Removed unnecessary things

--- index.php
+++ index.php
@@ -1,21 +1,10 @@
 <?php
 require_once 'app/init.php';
 include_once 'templates/header.php';
-
-
-$region = 'euw';
-$summoner = 'itanimulli';
-$url = BASE_URL . '/api/lol/' . $region .'/v1.4/summoner/by-name/' . $summoner . '?api_key=' . API_KEY;
-$request = file_get_contents($url);
-
-$data = json_decode($request);
 ?>
 
 <div class="row">
   <!-- Content -->
-  <?php
-  var_dump($data);
-  ?>
 </div>
 
 <?php include_once 'templates/footer.php'; ?>
