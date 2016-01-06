9db028ab8e02ecc626c6aa07aa40c96cf3bcad5e
quincy-soeliman <qsoeliman.dev@gmail.com>
2016-01-07 00:28:04 +0100

    Test for launching client

--- app/core/Spectator.php
+++ app/core/Spectator.php
@@ -3,13 +3,14 @@
 class Spectator extends Current_Match {
 
   public function launch_spectator_client() {
-    $command = '"C:\Riot Games\League of Legends\RADS\solutions\lol_game_client_sln\releases\0.0.1.74\
-      deploy\League of Legends.exe" ' . '"8394" ' . '"LoLLauncher.exe" ' . '"" ' . '"spectator spectator.euw1.lol.riotgames.com:80 ' .
-      $this->get_encryption_key() . ' ' . $this->get_current_game_id() . ' ' . $this->get_platform_id() . '"';
+    $command = "C:\\Riot Games\\League of Legends\\RADS\\solutions\\lol_game_client_sln\\releases\\0.0.1.74\\
+      deploy\\League of Legends.exe " . "8394 " . "LoLLauncher.exe " . "" . " spectator spectator.euw1.lol.riotgames.com:80 " .
+      $this->get_encryption_key() . " " . $this->get_current_game_id() . " " . $this->get_platform_id() . "";
 
     print $command;
 
-    exec($command . ' 2>&1');
+    exec($command . ' 2>&1', $output);
+    print_r($output);
   }
 
 }
